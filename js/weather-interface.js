0bd9bc192a634c3ea5317e0f3835513e011b60e1
colin bloom and jahan walsh <jahan@>
2017-04-17 16:02:31 -0700

    add api to app.

---
+++ js/weather-interface.js
@@ -0,0 +1,12 @@
+var apiKey = "d3744bfd6ee6988583aaafef1f4c9d05";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
