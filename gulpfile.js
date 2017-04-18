869dcac3a92f1622f1c90eecb3e1a25bf7db68b6
Colin Bloom and Ryan McAlpin <colin@>
2017-04-18 09:35:00 -0700

    install sass with gem install sass; npm install gulp-sass gulp-sourcemaps --save-dev, style weather box

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var lib = require('bower-files')({
   "overrides":{
     "bootstrap" : {
@@ -52,6 +54,7 @@ gulp.task("build", ['clean', 'jshint'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
 });
 
 
@@ -87,6 +90,7 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(['scss/*.scss'], ['cssBuild']);
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -99,4 +103,13 @@ gulp.task('bowerBuild', ['bower'], function(){
 
 gulp.task('htmlBuild', function() {
   browserSync.reload();
+});
+
+gulp.task('cssBuild', function(){
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
