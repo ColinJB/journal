8d2023f5af03700e0755f4f4ef4a5b6086433ebb
Colin Bloom <colinjbloom113@gmail.com>
2017-04-17 17:29:23 -0700

    add personal api key

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,6 +1,9 @@
 var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  currentWeatherObject.getWeather();
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
